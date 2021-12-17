f2471ceb5c3b7debca44269343829d6eac4b03dd
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2021-12-17 17:03:24 +0530

    Time: 3 ms (81.64%), Space: 46.8 MB (61.82%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(trust.length < n-1){
+             return -1; 
+        }
+        
+        
+        int[] indegree = new int[n+1];
+        int[] outdegree = new int[n+1];
+        
+        for(int[] relation : trust){
+            outdegree[relation[0]]++;
+            indegree[relation[1]]++;
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(indegree[i]== n-1 && outdegree[i]==0){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
