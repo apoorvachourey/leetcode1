2041ca170511e7cef4a9e2575cd44300b188725c
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-02-10 14:49:30 +0530

    Time: 2 ms (61.72%), Space: 73.5 MB (58.53%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -5,7 +5,7 @@ class Solution {
         */
         
         int current = nums[0];
-        int max = nums[0];
+        int max = current;
         
         for(int i=1;i<nums.length;i++){
             current  = Math.max(current+nums[i], nums[i]);
