5ed4361261bed3091dce36c6cb98bd9cbcebd9eb
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-02-11 09:33:07 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (7.90%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMajorityElement(int[] nums, int target) {
+        int firstOccurance = binarySearch(nums, target);
+        int plusNBy2Index = firstOccurance + nums.length/2;
+        if(plusNBy2Index < nums.length && nums[plusNBy2Index]==target) 
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public int binarySearch(int[] nums,int target){
+        int low =0;
+        int high = nums.length-1;
+        
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]<target) low=mid+1;
+            else if(nums[mid]>=target) high = mid;
+        }
+        
+        return low;
+    }
+}
