0e9e2ff653eb5e03c80256b7a8b2017706ddf9f7
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-02-13 18:56:20 +0530

    Time: 69 ms (75.24%), Space: 44.1 MB (69.91%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,42 +1,36 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-       Set<String> word=new HashSet(wordList);//it will be easy to check if wordlist contains that word
-        Queue<String> q=new LinkedList<>();
-        Set<String> visited=new HashSet<>();//to keep track of visited words;
-        q.add(beginWord);
-        visited.add(beginWord);
+        Set<String> set = new HashSet<>();
+        for(String s : wordList){
+            set.add(s);
+        }
+        if(!set.contains(endWord)){
+            return 0;
+        }
+        Set<String> visited=new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int level = 0; 
         
-        int level=0;
-        while(!q.isEmpty())
-        {
-            for(int i=q.size();i>0;i--)
-            {
-                String curr_word=q.poll();
-                if(curr_word.equals(endWord))
-                    return level+1;
-                
-                char[] c=curr_word.toCharArray();
-                for(int j=0;j<c.length;j++)//to replace every character of that word and check if it is present in wordlist
-                {
-                   char temp=c[j];
-                   for(char ch='a'; ch<='z'; ch++)
-                   {
-                       c[j]=ch;
-                       String word_formed=String.valueOf(c);//if this newly formed word isnt visited and also present in the wordlist add it to the queue
-                       if(!visited.contains(word_formed) && word.contains(word_formed))
-                       {
-                           q.add(word_formed);
-                           visited.add(word_formed);
-                       }
-                   }
-                    c[j]=temp;
+        while(!queue.isEmpty()){
+        int size = queue.size();
+        for(int i = 0; i < size; i++){
+            String cur = queue.remove();
+            if(cur.equals(endWord)){ return level + 1;}
+            for(int j = 0; j < cur.length(); j++){
+                char[] word = cur.toCharArray();
+                for(char ch = 'a'; ch <='z'; ch++){
+                    word[j] = ch;
+                    String check = new String(word);
+                    if(!check.equals(cur) && set.contains(check)){
+                        queue.add(check);
+                        set.remove(check);
+                    }
                 }
             }
-            
-            level++;//moving to the next level
         }
-        
-        return 0; 
+        level++;
     }
-    
+    return 0;
+    }
 }
