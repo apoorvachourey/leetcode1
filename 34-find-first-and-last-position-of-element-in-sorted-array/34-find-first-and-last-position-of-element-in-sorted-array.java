2b7bd75eb4826e666511caf1495fd5a0c37ad7fc
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-03-09 20:16:28 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (20.61%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int [] res = new int[2];
-        int s = search(nums,target);
-        if(s==-1) return new int[]{-1,-1};
-        res[0] = binarySearch(nums, target);
-        res[1] = binarySearchHigh(nums, target);
+        if(nums==null || nums.length==0) return new int[]{-1,-1};
+        int res [] = new int[2];
+        res[0]=findTheFirstOccurance(nums, target);
+        res[1]=findTheLastOccurance(nums,target);
         return res;
     }
     
-     public int search(int[] nums,int target){
-        int low =0;
-        int high = nums.length-1;
-        
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]<target) low=mid+1;
-            else if(nums[mid]>target) high = mid-1;
-            else if(nums[mid]==target) return mid;
-            
-        }
-        
-        return -1;
-    }
-    
-     public int binarySearch(int[] nums,int target){
-        int low =0;
+    public int findTheFirstOccurance(int nums[],int target){
+        int low = 0;
         int high = nums.length-1;
         
         while(low<high){
             int mid = low+(high-low)/2;
-            if(nums[mid]<target) low=mid+1;
-            else if(nums[mid]>=target) high = mid;
+            if(nums[mid]>=target){
+                high = mid;
+            }else{
+                low = mid+1;
+            }
         }
-        System.out.print(low);
-        return low;
+        if(nums[low]==target) return low;
+        return -1;
     }
     
-    public int binarySearchHigh(int[] nums,int target){
-        int low =0;
+     public int findTheLastOccurance(int nums[],int target){
+        int index = -1;
+        int low = 0;
         int high = nums.length-1;
         
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(nums[mid]>target) high = mid-1;
-            else if(nums[mid]<=target) low=mid+1;
-             
+            if(nums[mid]<=target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+            if(nums[mid]==target) index = mid;
         }
-        System.out.print(low);
-        return low-1;
+        
+         return index;
     }
+    
+    
 }
