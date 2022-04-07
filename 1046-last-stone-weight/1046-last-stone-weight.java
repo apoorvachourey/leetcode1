8a919de722800aa26c6a9e829f9f95146962a9ea
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-04-07 09:30:12 +0530

    Time: 2 ms (76.58%), Space: 41.7 MB (40.38%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,5 +1,6 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
+        if(stones.length==1) return stones[0];
         PriorityQueue<Integer> pqueue = new PriorityQueue<>((a,b)->(b-a));
         for(int x : stones){
             pqueue.add(x);
