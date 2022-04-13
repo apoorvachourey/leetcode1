168d44802e409a00eb72f1761601c4f051bbd584
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-04-13 19:59:55 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (81.84%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] res = new int[n][n];
-        int top =0; int left = 0; 
-        int right = n-1; int bottom = n-1;
-        int count = 1;
+        int res[][] = new int[n][n];
+        int top = 0; 
+        int left =0;
+        int right = n-1;
+        int bottom = n-1;
+        int count =1;
+        
         while(left<=right && top<=bottom){
+            
             for(int i=left; i<=right; i++){
-                res[top][i]=count++;
+                res[top][i] = count++;
             }
             top++;
             for(int i=top; i<=bottom; i++){
-                res[i][right]=count++;
-            }    
+                res[i][right] = count++;
+            }
             right--;
-            if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                    res[bottom][i]=count++;
-                }
-                bottom--;
+            for(int i=right; i>=left; i--){
+                res[bottom][i]= count++;
             }
-            if(left<=right){
-                for(int i=bottom;i>=top;i--){
-                    res[i][left]=count++;
-                }
-                left++;
+            bottom--;
+            for(int i=bottom; i>=top; i--){
+                res[i][left] = count++;
             }
+            left++;
+            
         }
+        
         return res;
     }
 }
