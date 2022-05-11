64e1b56b11e6c6bc77210fbc009b1f99652978c2
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-05-11 22:50:05 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (86.42%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        allSubsets(0,nums,result,new ArrayList<>());
+        recursiveSubsets(nums,0,new ArrayList<>(),result);
         return result;
     }
     
-    public void allSubsets(int index,int[] nums,List<List<Integer>> result, List<Integer> current){
+    public void recursiveSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result ){
+        
         if(index>=nums.length){
             result.add(new ArrayList<>(current));
             return;
         }
         
         current.add(nums[index]);
-        allSubsets(index+1,nums,result,current);
+        recursiveSubsets(nums,index+1,current,result);
         current.remove(current.size()-1);
-        allSubsets(index+1,nums,result,current);
+        recursiveSubsets(nums,index+1,current,result);
         
     }
 }
