14df25d281bf03fd01e2acec62182f41326df984
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-05-12 16:23:26 +0530

    Time: 1 ms (74.77%), Space: 41.5 MB (57.87%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        findCombinationSum(k,n,1,result,new ArrayList<>());
+        return result;
+    }
+    
+    public void findCombinationSum(int k, int sum,int start,List<List<Integer>> result , List<Integer> current){
+        if(current.size()==k && sum==0){
+            result.add(new ArrayList<>(current));
+            return ;
+        }
+        
+        for(int i=start; i<=9;i++){
+            current.add(i);
+            findCombinationSum(k,sum-i,i+1,result,current);
+            current.remove(current.size()-1);
+        }
+    }
+}
