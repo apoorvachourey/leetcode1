e6ca67b28ef2be2cb4e6854de227d862ba74afaf
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-05-13 18:08:34 +0530

    Time: 4 ms (21.73%), Space: 44.3 MB (38.58%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -3,27 +3,24 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
          Arrays.sort(nums);
         HashSet<List<Integer>> set = new HashSet<>();
-        helper(0, nums, result, new ArrayList<>(),set);
+        helper(0, nums, new ArrayList<>(),set);
          List<List<Integer>> newAns = new ArrayList<List<Integer>>();
-        for(List<Integer> curr : result){
-            set.add(curr);
-        }
         for(List<Integer> curr : set){
             newAns.add(curr);
         }
         return newAns;
     }
     
-    public void helper(int index, int[] nums, List<List<Integer>> result, List<Integer> current,HashSet<List<Integer>> set ){
+    public void helper(int index, int[] nums, List<Integer> current,HashSet<List<Integer>> set ){
         if(index>=nums.length){
-            result.add(new ArrayList<>(current));
+            set.add(new ArrayList<>(current));
             return;
         }
         
         current.add(nums[index]);
-        helper(index+1,nums,result,current,set);
+        helper(index+1,nums,current,set);
         current.remove(current.size()-1);
-        helper(index+1,nums,result,current,set);
+        helper(index+1,nums,current,set);
         
     }
 }
