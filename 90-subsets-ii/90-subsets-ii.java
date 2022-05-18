3754cbe3c0449176c27830b125dfb2f64b951e07
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-05-18 15:56:03 +0530

    Time: 2 ms (75.79%), Space: 43.7 MB (64.87%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,26 +1,19 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-         Arrays.sort(nums);
-        HashSet<List<Integer>> set = new HashSet<>();
-        helper(0, nums, new ArrayList<>(),set);
-         List<List<Integer>> newAns = new ArrayList<List<Integer>>();
-        for(List<Integer> curr : set){
-            newAns.add(curr);
-        }
-        return newAns;
+        Arrays.sort(nums);
+        findSubsetsWithDup(nums,0,result,new ArrayList<>());
+        return  result;
     }
     
-    public void helper(int index, int[] nums, List<Integer> current,HashSet<List<Integer>> set ){
-        if(index>=nums.length){
-            set.add(new ArrayList<>(current));
-            return;
+    public void findSubsetsWithDup(int[] nums,int index,  List<List<Integer>> result, List<Integer> current){
+        result.add(new ArrayList<>(current));
+        for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            current.add(nums[i]);
+            findSubsetsWithDup(nums,i+1,result,current);
+            current.remove(current.size()-1);
+            
         }
-        
-        current.add(nums[index]);
-        helper(index+1,nums,current,set);
-        current.remove(current.size()-1);
-        helper(index+1,nums,current,set);
-        
     }
 }
