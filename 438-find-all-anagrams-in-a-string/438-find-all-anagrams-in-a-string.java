0f552bf06f7355cc173482709aad8d7fc5d1d7b9
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2022-06-04 11:41:00 +0530

    Time: 40 ms (35.21%), Space: 49.2 MB (24.42%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,38 +1,42 @@
 class Solution {
-    static final int CHAR = 26; 
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        if(s.length()<p.length()) return res;
-        int [] countS = new int[CHAR];
-        int [] countP = new int[CHAR];
+        HashMap<Character,Integer> map = new HashMap<>();
         
+        //put pattern count in map
         for(int i=0;i<p.length();i++){
-            countS[s.charAt(i)-'a']++;
-            countP[p.charAt(i)-'a']++;
+            map.put(p.charAt(i),map.getOrDefault(p.charAt(i),0)+1);
         }
-         if(areSame(countS,countP)){
-                res.add(0);
-           }
-        
-        for(int i=p.length();i<s.length();i++){
-              
-            countS[s.charAt(i)-'a']++;
-            countS[s.charAt(i-p.length())-'a']--;
-              if(areSame(countS,countP)){
-                res.add(i-p.length()+1);
+        //a-1,b-1,c-1
+        int count = map.size();
+        //work on the window
+        int i=0;int j=0;
+        while(j<s.length()){
+            if(map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j),map.get(s.charAt(j))-1);
+                if(map.get(s.charAt(j))==0){
+                    count--;
+                }
             }
+            
+            if(j-i+1==p.length()){
+                if(count==0){
+                    res.add(i);
+                }
+           
+                
+                if(map.containsKey(s.charAt(i))){
+                        map.put(s.charAt(i),map.get(s.charAt(i))+1);
+                    if(map.get(s.charAt(i))==1)
+                        count++;
+                    
+                }
+                i++;
+            }
+            j++;
         }
         
         return res;
         
-    }
-    
-    public boolean areSame(int[] countS, int[] countP){
-        for(int i=0;i<CHAR;i++){
-            if(countS[i]!=countP[i]){
-                return false;
-            }
-        }
-        return true;
     }
 }
