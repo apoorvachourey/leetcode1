45831e20e107ab6fdd0b066649dc9452a75862c8
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2026-06-07 04:52:00 -0700

    Time: 44 ms (6.46%), Space: 131 MB (6.77%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,7 +1,7 @@
 class Solution {
     int totalRow;
     int totalCol;
-    int flag =0;
+    
     public int countSubIslands(int[][] grid1, int[][] grid2) {
          totalRow = grid2.length;
          totalCol = grid2[0].length;
@@ -9,29 +9,30 @@ class Solution {
         boolean visited [][] = new boolean[totalRow][totalCol];
         for(int row = 0; row<totalRow;row++){
             for(int col=0;col<totalCol;col++){
-             if(grid2[row][col]==1 && !visited[row][col]){
-                    flag=1; // when dfs succeeds
-                    dfs(row,col,grid1,grid2);
-                    subIslandsCount=subIslandsCount+flag;
+             if(grid2[row][col]==1 && !visited[row][col] && dfs(row,col,grid1,grid2,visited)){
+                    subIslandsCount++;
              }
             }
         }
         return subIslandsCount;
         
     }
-    private void dfs(int row, int col, int[][] grid1,int[][] grid2){
-        if(row<0 || row>=totalRow || col<0 || col>=totalCol || grid2[row][col]==0){
-            return;
+    private boolean dfs(int row, int col, int[][] grid1,int[][] grid2, boolean visited[][]){
+        boolean result = true;
+        if(row<0 || row>=totalRow || col<0 || col>=totalCol || grid2[row][col]==0 || visited[row][col]){
+            return true;
         }
+        visited[row][col] = true;
         if(grid1[row][col]==0){
-            flag=0;
+            result=false;
         }
         grid2[row][col]=0;
         int adj[][] = {{row-1,col},{row,col+1},{row+1,col},{row,col-1}};
         for(int neighbour[] : adj){
             int r = neighbour[0];
             int c = neighbour[1]; 
-            dfs(r,c,grid1,grid2);
+            result = dfs(r,c,grid1,grid2,visited) && result;
         }
+        return result;
     }
 }
