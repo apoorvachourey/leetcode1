16bb5d267583972c753a68a3ebec8dfe27c58a23
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2026-06-17 23:36:56 -0700

    Time: 4 ms (16.71%), Space: 49.9 MB (6.19%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,36 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int numOfProvinces = 0;
-        boolean visited[]=new boolean[isConnected.length];
-        for(int i=0;i<isConnected.length;i++){
+        int n = isConnected.length;
+        boolean visited[] = new boolean[n];
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i =0;i<n;i++){
+            graph.put(i, new ArrayList<>());
+        }
+        // create the adj List
+        for(int i= 0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    graph.get(i).add(j);
+                }
+            }
+        }
+    int count =0;
+        for(int i =0;i<n;i++){
             if(!visited[i]){
-                dfs(i,isConnected,visited);
-                numOfProvinces++;
+                dfs(i,graph,visited);
+                count++;
             }
         }
-        return numOfProvinces;
-    }
-    
+        return count;
+}
 
-    private void dfs(int node, int[][]isConnected,boolean[] visited){
-        visited[node]=true;
-        for(int i=0;i<isConnected.length;i++){
-            if(isConnected[node][i]==1 && !visited[i] ){
-                dfs(i,isConnected,visited);
-            }
+private void dfs(int node, HashMap<Integer, List<Integer>> graph, boolean visited[]){
+    visited[node]=true;
+    for(int neighbor : graph.get(node)){
+        if(!visited[neighbor]){
+            dfs(neighbor, graph, visited);
         }
     }
+}
+
 }
