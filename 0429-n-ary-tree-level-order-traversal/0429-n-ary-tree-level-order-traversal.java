5e9b5ded52bd1530ab69cfc06782afa5bd308f96
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2026-06-30 02:12:40 -0700

    Time: 3 ms (90.58%), Space: 47 MB (6.51%) - LeetHub

---
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) return res;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            while(size>0){
+                Node node = queue.poll();
+                level.add(node.val);
+                for(Node child : node.children){
+                    if(child!=null){
+                        queue.add(child);
+                    }
+                }
+            size--;
+            }
+            res.add(level);
+        }
+
+
+
+        return res;
+        
+    }
+}
