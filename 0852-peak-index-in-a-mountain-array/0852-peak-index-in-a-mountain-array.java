3ce705b4079586c97e9708ee970c90ffc0a43e7b
Apoorva Chourey <92981144+apoorvachourey@users.noreply.github.com>
2026-08-20 11:29:25 +0530

    Time: 0 ms (100%), Space: 80.3 MB (59.71%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+         int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid!=0 && mid!=n-1 && arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }else if(mid!=n-1 && arr[mid]<arr[mid+1]){ //increasing
+                start = mid+1;
+            }else{ //decreasing
+                end=mid-1;
+            }
+        }
+        return -1;//not reachable
+    }
+}
